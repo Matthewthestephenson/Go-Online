01b12d3a2630c99453a4176d201a6684a25e9134
prwalsh <prwalsh@iastate.edu>
2016-02-02 11:38:21 -0600

    Added Piece class

---
+++ Piece.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+class Piece
+{
+    String type;
+    Point position;
+
+    Piece(String n,Point p)
+    {
+        type=n;
+        position=p;
+    }
+
+    String getPosition()
+    {
+        return position.toString();
+    }
+
+    String getType()
+    {
+        return type;
+    }
+
+    String toString()
+    {
+        return getType()+" at "+getPosition();
+    }
+}
