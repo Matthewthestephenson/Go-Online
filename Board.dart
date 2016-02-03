58531bc6f4ae0fd4352dd84d4b7ce0211742ca93
Peter Thedens <pthedens@iastate.edu>
2016-02-02 19:36:42 -0600

    Added Board

---
+++ Board.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+abstract class Board
+{
+
+    // Need to make data storage for board - Make 2D array?
+
+    Board (int dimension)
+    {
+
+    }
+
+    // copy constructor
+    Board (Board oldBoard)
+    {
+
+    }
+
+    void placePiece (Points pt, Piece newPiece)
+    {
+
+    }
+
+    void printBoard ()
+    {
+
+    }
+}
