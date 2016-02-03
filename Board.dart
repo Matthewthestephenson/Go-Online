0a054e35530e9ccde42f9d6b34de06f7ba69eaa8
Peter Thedens <pthedens@iastate.edu>
2016-02-02 19:51:16 -0600

    created GoBoard.dart

--- Board.dart
+++ Board.dart
@@ -3,26 +3,21 @@ import 'dart:io';
 abstract class Board
 {
 
-    // Need to make data storage for board - Make 2D array?
+    // Need to make data storage for board - Make 2D array? or map?
 
     Board (int dimension)
     {
-
+        //TODO
     }
 
     // copy constructor
     Board (Board oldBoard)
     {
-
-    }
-
-    void placePiece (Point pt, Piece newPiece)
-    {
-
+        //TODO
     }
 
     void printBoard ()
     {
-
+        //TODO
     }
 }
---
+++ GoBoard.dart
@@ -0,0 +1,17 @@
+import 'dart.io';
+
+class GoBoard extends Board
+{
+    GoBoard()
+    {
+       //TODO
+    }
+
+    bool placePiece (Point pt, Piece newPiece)
+    {
+        //TODO
+        return false;
+    }
+
+
+}
