eaed8da639b56f1221e086452fff676456082df7
Austin Rohlfing <rohlfing@iastate.edu>
2016-02-22 19:11:12 -0600

    removed files incorrectly handled in last commit

--- testCode/JsonTest.java
+++ testCode/JsonTest.java
@@ -6,7 +6,7 @@ public class JsonTest{
       obj.put("Name", "Austin");
       obj.put("Year", "Sophomore");
       obj.put("Age", 19);
-      System.out.println(obj);
-      System.out.print("Key 'Age' has value: " + obj.get("Age").toString());
+      System.out.println("Object string is:" + obj.toString());
+      System.out.println("Key 'Age' has value: " + obj.get("Age").toString());
    }
 }
